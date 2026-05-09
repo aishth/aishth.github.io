19306a296c21d6cd8e91b9e8ca545e761cbc3ada
Aishwarya Thakur <51313834+aishth@users.noreply.github.com>
2026-05-09 22:02:01 +0200

    webpage v2

---
+++ assets/js/theme-toggle.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark")
+      ? "dark"
+      : "light"
+  );
+});
